85933842f8dd0d0b16d6fb3dbb00531892c4a62e
EmericBraud <ebraud95@gmail.com>
2025-11-22 00:07:03 +0100

    Added move generators (no bitboards) * Added move generator for 	* Rooks 	* Bishops 	* Kings 	* Pawns 	* Knights * Everything is covered by tests

--- include/move_generator.hpp
+++ include/move_generator.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
-
 #include "board.hpp"
 
-class MoveGenerator
+namespace MoveGen
 {
-    std::array<std::unordered_map<U64, std::vector<Move>>, N_PIECES_TYPE_HALF> move_tables;
-};
+    extern std::array<U64, BOARD_SIZE> KnightAttacks;
+    extern std::array<U64, BOARD_SIZE> KingAttacks;
+    extern std::array<U64, BOARD_SIZE> RookMasks;
+    extern std::array<U64, BOARD_SIZE> BishopMasks;
+    extern std::array<U64, BOARD_SIZE> PawnAttacksWhite;
+    extern std::array<U64, BOARD_SIZE> PawnAttacksBlack;
+    extern std::array<U64, BOARD_SIZE> PawnPushWhite;
+    extern std::array<U64, BOARD_SIZE> PawnPushBlack;
+    extern std::array<U64, BOARD_SIZE> PawnPush2White;
+    extern std::array<U64, BOARD_SIZE> PawnPush2Black;
+
+    void initialize_bitboard_tables();
+    void initialize_rook_masks();
+    void initialize_bishop_masks();
+    void initialize_pawn_masks();
+
+    std::vector<Move> generate_pseudo_legal_moves(const Board &board, Color color);
+
+    U64 generate_knight_moves(int from_sq, const Board &board);
+
+    U64 generate_rook_moves(int from_sq, const Board &board);
+}
--- include/utils.hpp
+++ include/utils.hpp
@@ -8,76 +8,90 @@
 #include <unordered_map>
 #include <cstdint>
 #include <utility>
+#include <climits>
 
+#define BOARD_SIZE 64
 #define STARTING_POS_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 using U64 = std::uint64_t;
 using bitboard = std::uint64_t;
 
+inline int get_lsb_index(U64 bb)
+{
+    return __builtin_ctzll(bb);
+}
+
+inline int pop_lsb(U64 &bb)
+{
+    int s = get_lsb_index(bb);
+    bb &= bb - 1;
+    return s;
+}
+
 enum class Square
 {
     a1,
-    a2,
-    a3,
-    a4,
-    a5,
-    a6,
-    a7,
-    a8, //
     b1,
-    b2,
-    b3,
-    b4,
-    b5,
-    b6,
-    b7,
-    b8, //
     c1,
-    c2,
-    c3,
-    c4,
-    c5,
-    c6,
-    c7,
-    c8, //
     d1,
-    d2,
-    d3,
-    d4,
-    d5,
-    d6,
-    d7,
-    d8, //
     e1,
-    e2,
-    e3,
-    e4,
-    e5,
-    e6,
-    e7,
-    e8, //
     f1,
-    f2,
-    f3,
-    f4,
-    f5,
-    f6,
-    f7,
-    f8, //
     g1,
-    g2,
-    g3,
-    g4,
-    g5,
-    g6,
-    g7,
-    g8, //
     h1,
+    a2,
+    b2,
+    c2,
+    d2,
+    e2,
+    f2,
+    g2,
     h2,
+    a3,
+    b3,
+    c3,
+    d3,
+    e3,
+    f3,
+    g3,
     h3,
+    a4,
+    b4,
+    c4,
+    d4,
+    e4,
+    f4,
+    g4,
     h4,
+    a5,
+    b5,
+    c5,
+    d5,
+    e5,
+    f5,
+    g5,
     h5,
+    a6,
+    b6,
+    c6,
+    d6,
+    e6,
+    f6,
+    g6,
     h6,
+    a7,
+    b7,
+    c7,
+    d7,
+    e7,
+    f7,
+    g7,
     h7,
-    h8, //
+    a8,
+    b8,
+    c8,
+    d8,
+    e8,
+    f8,
+    g8,
+    h8,
 };
