f8c4c08eb5e075d5f3e4f98837230fce0de42b60
Emeric Braud <emeric.braud@theodo.com>
2026-05-08 19:54:54 +0200

    improved refactoring logic by using generic Eval class

--- src/engine/eval/hce/pos_eval.cpp
+++ src/engine/eval/hce/pos_eval.cpp
@@ -1,4 +1,6 @@
-#include "pos_eval.hpp"
+#ifndef NNUE_EVAL
+
+#include "engine/eval/pos_eval.hpp"
 #include "engine/eval/hce/pawn_entry.hpp"
 
 #include "common/logger.hpp"
@@ -251,3 +253,4 @@ void Eval::print_pawn_stats()
     logs::debug << " - Misses: " << pawn_table.misses << std::endl;
     logs::debug << " - Rate:   " << pawn_table.get_hit_rate() << "%" << std::endl;
 }
+#endif
